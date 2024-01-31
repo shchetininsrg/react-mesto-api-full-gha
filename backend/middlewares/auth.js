4b76be5bdf997ff4ee35ce596c68e798027c7e1d
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2024-01-31 18:46:06 +0500

    fix jwtscrt

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 // const { UNAUTHORIZED } = require('../utils/constantErrors');
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = 'secret-key' } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
