fb67992da1823d9e97da2fb766a6d8df3fc59d29
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2024-01-31 18:17:25 +0500

    поменял конфиг апи

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000";
+const url = "https://api.schetininsrg.nomoredomainsmonster.ru/";
 const headers = {
   "Content-Type": "application/json",
 };
--- frontend/src/utils/configApi.js
+++ frontend/src/utils/configApi.js
@@ -1,3 +1,3 @@
 export const configApi = {
-	url: 'http://localhost:3000'
+	url: 'https://api.schetininsrg.nomoredomainsmonster.ru/'
 }
